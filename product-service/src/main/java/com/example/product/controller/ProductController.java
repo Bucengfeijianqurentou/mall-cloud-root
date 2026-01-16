f297b12a82e842388d9e5189dcaa146460ef511f
Bucengfeijianqurentou <2193008417@qq.com>
2026-01-16 10:19:39 +0800

    使用nacos配置中心，并整合bootstrap

--- product-service/src/main/java/com/example/product/controller/ProductController.java
+++ product-service/src/main/java/com/example/product/controller/ProductController.java
@@ -1,18 +1,37 @@
 package com.example.product.controller;
 
 import com.example.common.entity.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("/product")
+@RefreshScope //开启配置自动刷新
 public class ProductController {
+
+    // 读取 Nacos 里的配置
+    @Value("${nacos.feature.coupon:false}") // 冒号后面是默认值，防止Nacos没配报错
+    private boolean couponEnabled;
+
+    @Value("${nacos.feature.discount:1.0}")
+    private double discount;
+
+    @GetMapping("/config-test")
+    public Map<String, Object> testConfig() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("优惠券开关", couponEnabled);
+        map.put("当前折扣", discount);
+        return map;
+    }
 
     // 模拟数据库数据
     private static final Map<Long, Product> PRODUCT_DB = new ConcurrentHashMap<>();
